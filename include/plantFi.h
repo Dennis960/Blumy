f3bfd146574b242430cfae1f2f0a670d02909856
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-04-09 01:30:50 +0200

    changed scope of application

--- include/plantFi.h
+++ include/plantFi.h
@@ -1,13 +1,8 @@
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
-#include <rtcStore.h>
 #include <ESP8266WiFi.h>
-#include <PlantServer.h>
 
 #include <ArduinoJson.h>
-
-String UNDEFINED_NAME = "undefined";
-IPAddress subnet(255,255,255,0);
 
 class PlantFi
 {
@@ -15,26 +10,17 @@ private:
     HTTPClient http;
     WiFiClientSecure wifiClient;
 
-    String plants_url = "https://plants.hoppingadventure.com/api/plants";
-    String measure_url = "https://plants.hoppingadventure.com/api/plants/%d/measures";
+    String dataUrl = "https://esplant.hoppingadventure.com/api/data";
 public:
     PlantFi()
     {
-        Serial.print("Last SSID: ");
-        Serial.println(rtcStore.ssid);
         startWifiConnection();
     }
 
-    bool isWifiConnected()
-    {
-        if (WiFi.status() == WL_CONNECTED && WiFi.SSID() == rtcStore.ssid) {
-            Serial.println("WiFi connected");
-            return true;
-        }
-        Serial.println("Wifi not connected");
-        return false;
-    }
-
+    /**
+     * Waits until the wifi connection is established or a timeout occurs.
+     * @return True if the connection was established, false if a timeout occurred.
+    */
     bool waitUntilWifiConnected()
     {
         unsigned long startTime = millis();
@@ -44,92 +30,41 @@ public:
             wifiStatus = WiFi.status();
             if (millis()-startTime > 30000 || wifiStatus == WL_IDLE_STATUS || wifiStatus == WL_CONNECT_FAILED) // timeout after 30 seconds or on no connection found
             {
-                Serial.println("Connection failed");
                 return false;
             }
             delay(1); // delay to prevent esp watchdog errors
         }
-        Serial.print("Waiting for connection took: ");
-        Serial.println(millis() - startTime);
         return true;
     }
 
-    void startWifiConnection() // takes 4731 ms to connect without quick connect and 2007 with quick connect
+    /**
+     * Starts the wifi connection and waits until it is connected.
+     * Uses the SSID and PASSWORD defined in system environment variables as PIO_PASSWORD and PIO_SSID.
+    */
+    void startWifiConnection()
     {
         wifiClient.setInsecure();
-        Serial.print("Connecting to: ");
-        Serial.println(rtcStore.ssid);
-        if (isRtcValid())
-        {
-            Serial.println("Rtc is valid, using channel and bssid");
-            WiFi.begin(rtcStore.ssid, rtcStore.password, rtcStore.channel, rtcStore.bssid); // WiFi connection quick connect
-        }
-        else
-        {
-            Serial.println("Rtc is not valid, establishing new WiFi connection");
-            WiFi.begin(rtcStore.ssid, rtcStore.password); // WiFi connection without quick connect
-            waitUntilWifiConnected();
-            Serial.println("Saving channel and bssid");
-            rtcStore.channel = WiFi.channel();
-            setBssid(WiFi.BSSID()); // already writes current rtc, so no need to do it again
-        }
+        const char *ssid = WIFI_SSID;
+        const char *password = WIFI_PASSWORD;
+        WiFi.begin(ssid, password); // ssid and password are defined in secrets.h
+        waitUntilWifiConnected();
     }
 
-    int initPlantId()
+    /**
+     * Sends the sensor data to the api
+     * @param sensorAddress The address of the sensor
+     * @param water The water value of the sensor
+    */
+    void sendData(int sensorAddress, int water)
     {
-        Serial.println("Adding new plant");
-        http.begin(wifiClient, plants_url);
+        http.begin(wifiClient, dataUrl);
         http.addHeader("Content-Type", "application/json");
-        String requestBody = "{\"name\":\"" + UNDEFINED_NAME + "\"}";
-        int httpResponseCode = http.POST(requestBody);
-        String payload = http.getString();
-        Serial.println(plants_url);
-        Serial.println(requestBody);
-        Serial.println(payload);
-        Serial.println(httpResponseCode);
-        http.end();
-        
-        if (httpResponseCode == HTTP_CODE_CREATED)
-        {
-            DynamicJsonDocument doc(1024);
-            deserializeJson(doc, payload);
-            Serial.println((int)doc["id"]);
-            return doc["id"];
-        }
-        return -1;
-    }
-
-    void startConnectionToPlantApi()
-    {
-        char url[128];
-        sprintf(url, measure_url.c_str(), rtcStore.plant_id);
-        http.begin(wifiClient, url);
-        http.addHeader("Content-Type", "application/json");
-    }
-
-    void sendMeasurement(unsigned int water, unsigned int sun, float voltage)
-    {
-        Serial.println("Sending measurement");
-        startConnectionToPlantApi();
-        char requestBody[128];
-        sprintf(requestBody, "{\"water\":%d,\"sun\":%d,\"voltage\":%f}", water, sun, voltage);
-        Serial.println(requestBody);
-
-        http.POST(requestBody);
-        Serial.println(http.getString());
-        http.end();
-    }
-
-    void sendReferenceCapacitance(unsigned int referenceCapacitance)
-    {
-        Serial.println("Sending measurement");
-        startConnectionToPlantApi();
-        char requestBody[64];
-        sprintf(requestBody, "{\"referenceWater\":%d}", referenceCapacitance);
-        Serial.println(requestBody);
-
-        http.POST(requestBody);
-        Serial.println(http.getString());
+        StaticJsonDocument<200> doc;
+        doc["sensorAddress"] = sensorAddress;
+        doc["water"] = water;
+        String payload;
+        serializeJson(doc, payload);
+        http.POST(payload);
         http.end();
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,60 @@
 #include <plantFi.h>
 #include <sensor.h>
+#include <ESP8266WiFi.h>
 
-#ifdef ESP_01
-#define SENSOR_ENABLE_PIN 3
-#else
-#define SENSOR_ENABLE_PIN 0
-#endif
-
-ADC_MODE(ADC_VCC);
-
-int sensor_address = 0x20;
-bool isDoubleReset = false;
+const int sdaPin = 5;
+const int sclPin = 4;
+const uint64_t SLEEP_DURATION = 3600000000; // 60 minutes
+#define DEFAULT_ADDRESS 127
 
 void startDeepSleep(uint64_t duration)
 {
-  Serial.print("Sleeping for ");
-  Serial.print(duration/1000000);
-  Serial.println(" seconds");
   ESP.deepSleep(duration);
   yield();
 }
 
-float getVoltage()
-{
-  return ESP.getVcc()/1000.00;
-}
-
-void enableSensor()
-{
-  pinMode(SENSOR_ENABLE_PIN, OUTPUT);
-  digitalWrite(SENSOR_ENABLE_PIN, HIGH); // enable sensor
-}
-
-void disableSensor()
-{
-  digitalWrite(SENSOR_ENABLE_PIN, LOW); // disable sensor
-}
-
 void setup()
 {
-  unsigned long startTime = millis();
-  enableSensor(); // enable as early as possible to have as little delay as possible
-  Serial.begin(115200); // Serial connection
+  PlantFi plantFi = PlantFi();
 
-  readFromRTC();
-  isDoubleReset = rtcStore.isDoubleReset;
-  rtcStore.isDoubleReset = true;
-  writeToRTC();
+  Wire.begin(sdaPin, sclPin);
+  // Use a clock stretch limit of 4ms to prevent the esp from crashing when the sensor is not connected
+  Wire.setClockStretchLimit(4000);
 
-  Serial.println(rtcStore.plant_id);
-  Serial.println("");
-  PlantFi plantFi; // this will start to setup a WiFi connection, it takes at least 1.5 seconds async before wifi is connected, init method takes 4 ms
-  Sensor sensor(sensor_address);
-  while (millis() - startTime < 80) // Sensor takes 70 ms to boot after getting power
+  // Scan for sensors and save their addresses
+  int sensorAddresses[127];
+  int sensorCount = 0;
+  int highestAddress = 0;
+  for (int sensorAddress = 1; sensorAddress < 128; sensorAddress++)
   {
-    delay(1);
-  }
-  sensor.requestLight(); // reqeust light before wifi setup to have enough time to measure
-  unsigned int water = sensor.readCapacitance(); // has a 550 ms delay
-
-  unsigned int sun = sensor.readLight(); // has a 550 ms delay
-  sensor.chirpIfDry();
-  // by now 1500 - 1600 ms have passed
-  disableSensor(); // disable sensor pin to maybe save tiny amounts of power for the next 2 seconds
-
-  if (!plantFi.waitUntilWifiConnected()) // after getting all sensor data, wait for the wifi connection to finish
-  {
-    
-    startPlantServer(); // this should set ssid and password
-    plantFi.startWifiConnection();
-    plantFi.waitUntilWifiConnected();
-    if (!isPlantIdInit) {
-        rtcStore.plant_id = plantFi.initPlantId();
+    Wire.beginTransmission(sensorAddress);
+    if (Wire.endTransmission() == 0)
+    {
+      sensorAddresses[sensorCount] = sensorAddress;
+      if (sensorAddress == DEFAULT_ADDRESS)
+      {
+        // The default address is used by new sensors, so we need to change it
+        Sensor sensor = Sensor(sensorAddress);
+        sensor.setAddress(highestAddress + 1);
+      }
+      if (sensorAddress > highestAddress)
+      {
+        highestAddress = sensorAddress;
+      }
+      sensorCount++;
     }
   }
-  
-  if (isDoubleReset)
+
+  for (int i = 0; i < sensorCount; i++)
   {
-    Serial.println("Calibration reset");
-    sensor.setLastCapacitanceAsReference();
-    sensor.chirpTwice(); // chirp twice to show that reference capacitance is measured
-    plantFi.sendReferenceCapacitance(water);
+    Sensor sensor = Sensor(sensorAddresses[i]);
+    unsigned int water = sensor.readCapacitance(); // has a 550 ms delay
+    plantFi.sendData(sensorAddresses[i], water);
   }
-  else
-  {
-    Serial.println("Deep sleep reset");
-  }
-
-  plantFi.sendMeasurement(water, sun, getVoltage());
-
-  rtcStore.isDoubleReset = false;
-  writeToRTC();
-
-  Serial.print("Cycle took: ");
-  Serial.println(millis() - startTime);
-  startDeepSleep(sensor.getSleepDuration());
+  startDeepSleep(SLEEP_DURATION);
 }
 
 void loop()
 {
-  // empty currently
+  // nothing to do here
 }
