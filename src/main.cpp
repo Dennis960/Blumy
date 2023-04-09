916c06f2542f38fdfb4a10aed23f4b7d275aff60
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-04-09 14:53:41 +0200

    added debug option

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,15 @@
 #include <sensor.h>
 #include <ESP8266WiFi.h>
 
-const int sdaPin = 5;
-const int sclPin = 4;
+#define DEBUG 0
+
+const int sdaPin = 5; // D1 on NodeMCU
+const int sclPin = 4; // D2 on NodeMCU
+#if DEBUG
+const uint64_t SLEEP_DURATION = 10000000; // 10 seconds
+#else
 const uint64_t SLEEP_DURATION = 3600000000; // 60 minutes
-#define DEFAULT_ADDRESS 127
+#endif
 
 void startDeepSleep(uint64_t duration)
 {
@@ -15,41 +20,52 @@ void startDeepSleep(uint64_t duration)
 
 void setup()
 {
-  PlantFi plantFi = PlantFi();
+  #if DEBUG
+  Serial.begin(115200);
+  Serial.println();
+  #endif
 
   Wire.begin(sdaPin, sclPin);
   // Use a clock stretch limit of 4ms to prevent the esp from crashing when the sensor is not connected
   Wire.setClockStretchLimit(4000);
 
   // Scan for sensors and save their addresses
+  #if DEBUG
+  Serial.println("Scanning for sensors...");
+  #endif
   int sensorAddresses[127];
   int sensorCount = 0;
-  int highestAddress = 0;
   for (int sensorAddress = 1; sensorAddress < 128; sensorAddress++)
   {
     Wire.beginTransmission(sensorAddress);
     if (Wire.endTransmission() == 0)
     {
+      #if DEBUG
+      Serial.print("Found sensor at address ");
+      Serial.println(sensorAddress);
+      #endif
       sensorAddresses[sensorCount] = sensorAddress;
-      if (sensorAddress == DEFAULT_ADDRESS)
-      {
-        // The default address is used by new sensors, so we need to change it
-        Sensor sensor = Sensor(sensorAddress);
-        sensor.setAddress(highestAddress + 1);
-      }
-      if (sensorAddress > highestAddress)
-      {
-        highestAddress = sensorAddress;
-      }
       sensorCount++;
     }
   }
+  PlantFi plantFi = PlantFi();
+  #if DEBUG
+  Serial.println("Starting WiFi...");
+  #endif
 
   for (int i = 0; i < sensorCount; i++)
   {
     Sensor sensor = Sensor(sensorAddresses[i]);
     unsigned int water = sensor.readCapacitance(); // has a 550 ms delay
+    #if DEBUG
+    Serial.print("Sending data for sensor ");
+    Serial.print(sensorAddresses[i]);
+    Serial.print(" with water level ");
+    Serial.println(water);
+    sensor.chirp();
+    #else
     plantFi.sendData(sensorAddresses[i], water);
+    #endif
   }
   startDeepSleep(SLEEP_DURATION);
 }
