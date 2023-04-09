0ba78c0659a4ab7710a4fdf507bf8cd64385d28f
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-04-09 20:58:54 +0200

    renamed frontend and backend to app and api

---
+++ esplant-server/deploy.py
@@ -0,0 +1,70 @@
+import pysftp
+import os
+import sys
+import subprocess
+
+local_path = os.path.dirname(os.path.realpath(__file__)).replace('\\', '/')
+remote_path = '/var/www/hoppingadventure/hopping/esplant'
+frontend_name = 'app'
+backend_name = 'api'
+frontend_param = 'f'
+backend_param = 'b'
+
+def try_mkdir(sftp: pysftp.Connection, folder: str):
+    try:
+        sftp.mkdir(folder)
+    except OSError:
+        pass
+
+# recursive put function for pysftp to upload a directory windows
+def put_r_portable(sftp: pysftp.Connection, localdir: str, remotedir: str):
+    for entry in os.listdir(localdir):
+        remotepath = remotedir + "/" + entry
+        localpath = os.path.join(localdir, entry)
+        if not os.path.isfile(localpath):
+            try_mkdir(sftp, remotepath)
+            put_r_portable(sftp, localpath, remotepath)
+        else:
+            sftp.put(localpath, remotepath)
+
+def npm_build(folder: str):
+    print('  Building ' + folder)
+    subprocess.call('npm run build', shell=True, cwd=local_path + '/' + folder)
+
+def install_node_modules(sftp: pysftp.Connection, folder: str):
+    print('  Installing node modules')
+    sftp.execute('cd ' + remote_path + "/" + folder + ' && npm install')
+
+def upload_folder(sftp: pysftp.Connection, folder: str, target_folder: str = None):
+    if target_folder == None:
+        target_folder = folder
+    print('  Uploading ' + folder + ' to ' + target_folder)
+    try_mkdir(sftp, remote_path + '/' + target_folder)
+    put_r_portable(sftp, local_path + '/' + folder, remote_path + '/' + target_folder)
+
+def restart_esplant_service(sftp: pysftp.Connection):
+    print('  Restarting esplant service')
+    sftp.execute('sudo systemctl restart esplant.service')
+
+args = ''.join(sys.argv[1:])
+
+with pysftp.Connection('hoppingadventure.com', username='hopping', private_key="C:/Users/Dennis/Documents/FileZilla/hopping.key") as sftp:
+    if backend_param in args:
+        print('Deploying ' + backend_name)
+        npm_build(backend_name)
+        upload_folder(sftp, backend_name + '/dist', backend_name)
+        install_node_modules(sftp, backend_name)
+        print('Restarting esplant service')
+        restart_esplant_service(sftp)
+    else:
+        print('Skipping ' + backend_name + ' deploy,      enable with "' + backend_param + '"')
+    
+    if frontend_param in args:
+        print('Deploying ' + frontend_name)
+        npm_build(frontend_name)
+        upload_folder(sftp, backend_name + '/' + frontend_name)
+    else:
+        print('Skipping ' + frontend_name + ' deploy, enable with "' + frontend_param + '"')
+
+
+print('Done!')
