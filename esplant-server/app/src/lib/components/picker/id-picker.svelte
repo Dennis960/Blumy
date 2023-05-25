c65d0a1cc8608abc3c9bf27a79bfb1d3f4e817b5
Dennis960 <dennis.haumann03@gmail.com>
2023-05-25 19:52:10 +0200

    esplant-server: added labels

--- esplant-server/app/src/lib/components/picker/id-picker.svelte
+++ esplant-server/app/src/lib/components/picker/id-picker.svelte
@@ -3,7 +3,7 @@
 
 	export let id = 1;
 	let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-	let valueDescriptions = values.map((value) => value.toString());
+	let valueDescriptions = ["1 Bohnen draußen", "2 Test", "3 Drachenpalme", "4 Blume Oben", "5 Bohne Dennis", "6 Zucchini draußen", "7 Pflücksalat", "8 nicht belegt", "9 nicht belegt", "10 nicht belegt"];
 </script>
 
 <Picker {values} bind:value={id} {valueDescriptions} icon={'Sensor.png'} />
