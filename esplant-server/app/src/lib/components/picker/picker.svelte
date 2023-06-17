4e638217c7f6328992f2937d449a1ffc7734a7f3
Denni960 <dennis.haumann03@gmail.com>
2023-06-17 10:07:01 +0200

    app: added scrollable pickers

--- esplant-server/app/src/lib/components/picker/picker.svelte
+++ esplant-server/app/src/lib/components/picker/picker.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
 	import Icon from '$lib/components/icon.svelte';
+    import { onMount } from 'svelte';
 
 	export let values: any[];
 	export let valueDescriptions: any[];
 	export let value: any;
 	export let color: string | undefined = undefined;
 	export let icon: string | undefined = undefined;
+	let pickerContainer: HTMLElement;
+
+	onMount(() => {
+		pickerContainer.addEventListener('wheel', (e) => {
+			const delta = Math.sign(e.deltaY);
+			const index = values.indexOf(value);
+			if (index !== -1) {
+				const newIndex = index + delta;
+				if (newIndex >= 0 && newIndex < values.length) {
+					value = values[newIndex];
+				}
+			}
+			e.preventDefault();
+		});
+	})
 </script>
 
-<div class="picker-container">
+<div class="picker-container" bind:this={pickerContainer}>
 	<div class="icon">
 		{#if icon}
 			<Icon size={25} {icon} />
