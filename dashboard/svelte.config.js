22ccc384a08778e33372c9ee22656d0bf186306c
Timo <timo@bredex.de>
2023-06-23 17:36:14 +0200

    dashboard: Configure static build

--- dashboard/svelte.config.js
+++ dashboard/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,14 +8,15 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: '../esplant-server/api/app',
+			assets: '../esplant-server/api/app',
+			fallback: 'index.html',
+		}),
 		alias: {
 			$lib: './src/lib',
-			$components: './src/lib/components',
-		},
+			$components: './src/lib/components'
+		}
 	}
 };
 
