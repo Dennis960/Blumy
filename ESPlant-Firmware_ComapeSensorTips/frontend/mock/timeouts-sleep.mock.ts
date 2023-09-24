e420733afcd4ead62ddc31d9d2a0c001bc44f232
Dennis960 <dennis.haumann03@gmail.com>
2023-09-24 12:29:34 +0200

    Firmware_CompareSensorTips: Add README

--- ESPlant-Firmware_ComapeSensorTips/frontend/mock/timeouts-sleep.mock.ts
+++
@@ -1,8 +0,0 @@
-import { defineMock } from "vite-plugin-mock-dev-server";
-
-export default defineMock(
-    {
-        url: "/api/timeouts/sleep",
-        body: 1800000,
-    }
-);
---
+++ ESPlant-Firmware_CompareSensorTips/README.md
@@ -0,0 +1,3 @@
+# ESPlant-Firmware_CompareSensorTips
+
+This folder was used to compare different PCB sensor tip layouts
