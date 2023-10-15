a25f36a887492290c3d627bd68f78f9c0d4c4f96
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-10-15 13:36:25 +0000

    Case: wip

--- ESPlant-Case/v3/main.py
+++ ESPlant-Case/v3/main.py
@@ -2,7 +2,7 @@ try:
     from ocp_vscode import show_object
 except ImportError:
     show_object = lambda *any: None
-from utils import load_parts, extrude_part, extrude_part_width, extrude_part_height
+from utils import load_parts, extrude_part_faces, extrude_part_width, extrude_part_height
 from components import battery_springs
 import cadquery as cq
 from board_converter import convert_if_needed
@@ -25,17 +25,21 @@ case_hole_extrusion_size = 50
 
 HOLE = None
 parts_to_exclude = ["PinHeader"]
-parts_to_keep_original_shape = ["ESPlant-Board_PCB"]
-parts_to_extrude = [
+parts_to_keep_original_shape = ["PCB"]
+parts_to_extrude_for_case_bottom = [
+    {"name": "", "direction": ">Z", "length": board_tolerance_z},
+    {"name": "", "direction": "<Z", "length": board_tolerance_z},
     {"name": "MICRO-USB", "direction": ">X", "length": HOLE, "min_width": 11, "min_height": 6.5},
-    {"name": "SW-SMD", "direction": ">Z", "length": HOLE},
+    {"name": "SW-SMD_4P", "direction": ">Z", "length": HOLE},
+    {"name": "SW-SMD_MK", "direction": ">Z", "length": HOLE, "offset_y": -2, "min_height": 10},
     {"name": "LED", "direction": ">Z", "length": HOLE},
     {"name": "ALS-PT19","direction": ">Z", "length": HOLE},
-    {"name": "", "direction": ">Z", "length": board_tolerance_z},
-    {"name": "", "direction": "<Z", "length": board_tolerance_z}
+    {"name": "PCB","direction": "<Z", "length": HOLE},
+    {"name": "ESP","direction": "<Z", "length": HOLE},
+    {"name": "ESP","direction": ">Z", "length": 2},
 ]
 
-case_height = 60 # TODO this might not be the correct height
+case_height = 62
 
 ###----------------- Board + Components (Original) -----------------###
 board = cq.importers.importStep("ESPlant-Case/v3/ESPlant-Board.step")
@@ -47,30 +51,36 @@ parts_raw, parts_boxes, parts_names = load_parts(parts_exclude=parts_to_exclude)
 
 def part_indices_of(name_re: str):
     """
-    Get the indices of all parts that match the given regular expression.
+    Get the indices of all parts that include the given regular expression.
     """
-    return [i for i, part_name in enumerate(parts_names) if re.match(name_re, part_name)]
+    return [i for i, part_name in enumerate(parts_names) if re.match(f".*{name_re}.*", part_name)]
 
 for i in range(len(parts_raw)):
     if any(part_keep_shape in parts_names[i] for part_keep_shape in parts_to_keep_original_shape):
-        parts_boxes[i] = extrude_part("<Z", parts_raw[i], board_tolerance_z)
+        parts_boxes[i] = extrude_part_faces("<Z", parts_raw[i], board_tolerance_z)
     else:
         parts_boxes[i] = parts_boxes[i].union(parts_boxes[i].faces("<Z").shell(part_tolerance, kind="intersection"))
 
 parts_hole_extrusions = []
 
-for part_to_extrude in parts_to_extrude:
+for part_to_extrude in parts_to_extrude_for_case_bottom:
     part_name = part_to_extrude["name"]
     extrude_dir = part_to_extrude["direction"]
     is_hole_extrusion = part_to_extrude["length"] is HOLE
     extrude_len = case_hole_extrusion_size if is_hole_extrusion else part_to_extrude["length"]
     part_indices = part_indices_of(part_name)
     for part_index in part_indices:
-        extrusion = extrude_part(extrude_dir, parts_boxes[part_index], extrude_len)
+        extrusion = extrude_part_faces(extrude_dir, parts_boxes[part_index], extrude_len)
         if "min_width" in part_to_extrude:
             extrusion = extrude_part_width(extrusion, part_to_extrude["min_width"], extrude_dir)
         if "min_height" in part_to_extrude:
             extrusion = extrude_part_height(extrusion, part_to_extrude["min_height"], extrude_dir)
+        if "offset_x" in part_to_extrude:
+            extrusion = extrusion.translate((part_to_extrude["offset_x"], 0, 0))
+        if "offset_y" in part_to_extrude:
+            extrusion = extrusion.translate((0, part_to_extrude["offset_y"], 0))
+        if "offset_z" in part_to_extrude:
+            extrusion = extrusion.translate((0, 0, part_to_extrude["offset_z"]))
         if is_hole_extrusion:
             parts_hole_extrusions.append(extrusion)
         else:
@@ -80,17 +90,24 @@ for part_to_extrude in parts_to_extrude:
 part_union = cq.Workplane("XY")
 for part in parts_boxes:
     part_union = part_union.union(part)
+part_union = part_union.union(battery_springs)
 
 ###----------------- Case -----------------###
 # get bounding box of all parts
 part_union_bounding_box = part_union.val().BoundingBox()
 part_union_center = part_union.val().CenterOfBoundBox()
 part_union_box = cq.Workplane("XY").box(part_union_bounding_box.xlen, case_height, part_union_bounding_box.zlen).translate((part_union_center.x, part_union_center.y, part_union_center.z))
+part_union_box = part_union_box.translate((0, part_union_bounding_box.ylen / 2 - case_height/2, 0))
 part_union_shell = part_union_box.faces("<Z").shell(minimum_wall_thickness, kind="intersection")
-# subtract part_union and part_hole_extrusions from case
+
+part_union_shell = part_union_shell.union(extrude_part_faces("<Z", part_union_shell, 8, faces_selector=">Z[-2]"))
+
+# cut out holes and parts
 part_union_shell = part_union_shell.cut(part_union)
 for part_hole_extrusion in parts_hole_extrusions:
     part_union_shell = part_union_shell.cut(part_hole_extrusion)
+
+# TODO use holes to fixate the case
 
 ###----------------- Preview -----------------###
 # for part_name, bounding_box_part in zip(part_names, part_bounding_boxes):
@@ -99,11 +116,7 @@ for part_hole_extrusion in parts_hole_extrusions:
 show_object(board, name="board")
 # show_object(parts, name="parts")
 # show_object(part_union, name="part_union")
-show_object(part_union_shell, name="part_union_shell")
+show_object(part_union_shell, name="case_bottom")
 
 ###----------------- Export -----------------###
-objects = parts_boxes + [battery_springs]
-assembly = cq.Assembly()
-for obj in objects:
-    assembly.add(obj)
-assembly.save(filename)
+cq.Assembly(part_union_shell).save(filename)
