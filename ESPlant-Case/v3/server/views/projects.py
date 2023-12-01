b952dec43d98594217de97c46bc776d267970df1
Timo <timo@bredex.de>
2023-12-01 08:58:58 +0000

    Implement versioned cases and download

--- ESPlant-Case/v3/server/views/projects.py
+++ ESPlant-Case/v3/server/views/projects.py
@@ -2,9 +2,8 @@ import os
 import glob
 import uuid
 import shutil
-import pickle
 import zipfile
-from flask import Blueprint, request, session, render_template, url_for, redirect
+from flask import Blueprint, request, render_template, url_for, redirect, send_file
 from werkzeug.utils import secure_filename
 from cq_part import PartSetting, HOLE_TYPE, DIMENSION_TYPE, ALIGNMENT
 
@@ -13,31 +12,31 @@ from .. import utils
 from .. import tasks
 
 
-# TODO unsafe - do not use pickle and do not store this on the client
-def read_config() -> parameters.CaseConfiguration:
-    return pickle.loads(session["config"])
+def get_version():
+    return int(request.args.get("version", 1))
 
-def write_config(c: parameters.CaseConfiguration):
-    session["config"] = pickle.dumps(c)
+def incremented_version():
+    return int(request.args.get("version", 0)) + 1
 
 
-bp = Blueprint("wizard", __name__, url_prefix="/wizard")
+bp = Blueprint("projects", __name__, url_prefix="/projects")
 
-@bp.route("/upload")
+@bp.route("/new")
 def upload():
-    return render_template("./pages/wizard/upload.html")
+    return render_template("./pages/projects/upload.html")
 
 
-@bp.route("/board", methods=["POST"])
+@bp.route("/new", methods=["POST"])
 def upload_board():
     file = request.files["file"]
 
     if not file:
         return 'No file selected!'
 
-    file_uuid = str(uuid.uuid4())
+    project_id = str(uuid.uuid4())
+
     filename = secure_filename(file.filename)
-    directory_path = utils.get_upload_directory(file_uuid)
+    directory_path = utils.get_project_directory(project_id)
     os.makedirs(directory_path)
     file_path = os.path.join(directory_path, filename)
     file.save(file_path)
@@ -62,7 +61,6 @@ def upload_board():
             return 'No Kicad PCB file was found in the archive!'
 
     tasks.run_convert.delay(directory_path, filename)
-    session["file"] = file_uuid
 
     pcb_tolerance=(1.5, 1.5, 0.5)
     config = parameters.CaseConfiguration(
@@ -90,18 +88,25 @@ def upload_board():
         bottom_case_offset=(0, ALIGNMENT.POSITIVE, 0),
         list_of_additional_parts=[]
     )
-    write_config(config)
-    return redirect(url_for("wizard.view_board"))
+
+    version = incremented_version()
+    utils.write_config(project_id, version, config)
+    return redirect(url_for("projects.view_board", project_id=project_id, version=version))
 
 
-@bp.route("/board")
-def view_board():
-    return render_template("./pages/wizard/board.html", glb_path=url_for("files.serve_board", id=session["file"]))
+@bp.route("/<project_id>/board")
+def view_board(project_id: str):
+    version = get_version()
+    return render_template("./pages/projects/board.html",
+        project_id=project_id,
+        version=version,
+        glb_path=url_for("projects.board_glb", project_id=project_id, version=version),
+    )
 
 
-@bp.route("/parameters", methods=["POST"])
-def set_parameters():
-    previous_config = read_config()
+@bp.route("/<project_id>/parameters", methods=["POST"])
+def set_parameters(project_id: str):
+    previous_config = utils.read_config(project_id, get_version())
 
     case_wall_thickness = float(request.form['case_wall_thickness'])
     case_floor_height = float(request.form['case_floor_height'])
@@ -130,39 +135,72 @@ def set_parameters():
         previous_config.list_of_additional_parts
     )
 
-    write_config(config)
-
-    return redirect(url_for("wizard.view_parameters"))
-
-
-@bp.route("/parameters")
-def view_parameters():
-    return render_template("./pages/wizard/parameters.html", config=read_config())
+    version = incremented_version()
+    utils.write_config(project_id, version, config)
+    return redirect(url_for("projects.view_parameters",
+        project_id=project_id,
+        version=version
+    ))
 
 
-@bp.route("/bottom-case", methods=["POST"])
-def generate_bottom_case():
-    id = session["file"]
-
-    if id is None:
-        return "Does not exist"
-
-    root_path = utils.get_upload_directory(id)
-
-    # TODO instead of overwriting, create versioned files
-    export_path = os.path.join(root_path, "bottom-case.glb")
-    if os.path.exists(export_path):
-        os.remove(export_path)
-
-    tasks.run_generate_bottom_case.delay(root_path, read_config())
-    return redirect(url_for("wizard.view_bottom_case"))
+@bp.route("/<project_id>/parameters")
+def view_parameters(project_id: str):
+    version = get_version()
+    return render_template("./pages/projects/parameters.html",
+        project_id=project_id,
+        version=version,
+        config=utils.read_config(project_id, version)
+    )
 
 
-@bp.route("/bottom-case")
-def view_bottom_case():
-    return render_template("./pages/wizard/bottom_case.html", glb_path=url_for("files.serve_bottom_case", id=session["file"]))
+@bp.route("/<project_id>/bottom-case", methods=["POST"])
+def generate_bottom_case(project_id: str):
+    version = get_version()
+    tasks.run_generate_bottom_case.delay(project_id, version)
+    return redirect(url_for("projects.view_bottom_case", project_id=project_id, version=version))
 
 
-@bp.route("/finish", methods=["POST"])
-def confirm():
-    return ":)"
+@bp.route("/<project_id>/bottom-case")
+def view_bottom_case(project_id: str):
+    version = get_version()
+    return render_template("./pages/projects/bottom_case.html",
+        project_id=project_id,
+        version=version,
+        glb_path=url_for("projects.bottom_case", format="glb", project_id=project_id, version=version)
+    )
+
+
+@bp.route("/<project_id>/download", methods=["POST"])
+def confirm(project_id: str):
+    return redirect(url_for("projects.view_download", project_id=project_id, version=get_version()))
+
+
+@bp.route("/<project_id>/download")
+def view_download(project_id: str):
+    return render_template("./pages/projects/download.html",
+        project_id=project_id,
+        version=get_version()
+    )
+
+
+@bp.route("/<project_id>/board.glb")
+def board_glb(project_id: str):
+    file_path = utils.get_project_export_glb_path(project_id)
+
+    if not os.path.exists(file_path):
+        return "Does not exist on disk", 404
+
+    return send_file(file_path)
+
+
+@bp.route("/<project_id>/bottom-case.<format>")
+def bottom_case(project_id: str, format: str):
+    if format not in ['glb', 'stl', 'step']:
+        return "Invalid file format", 400
+
+    file_path = utils.get_project_version_export_base_path(project_id, get_version()) + "." + format
+
+    if not os.path.exists(file_path):
+        return "Does not exist on disk", 404
+
+    return send_file(file_path)
