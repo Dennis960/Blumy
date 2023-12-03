fd6e14757c787cf91d6f097db559f2a31519cc96
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-12-03 20:52:16 +0000

    Server: Remove session

--- ESPlant-Server/api/middlewares/passport.ts
+++ ESPlant-Server/api/middlewares/passport.ts
@@ -6,27 +6,27 @@ import { knex } from "../config/knex.js";
 
 const SESSION_SECRET = process.env.SESSION_SECRET!;
 if (SESSION_SECRET == undefined) {
-  throw new Error("SESSION_SECRET must be set");
+  console.error("SESSION_SECRET must be set");
 }
 
 const router = Router();
 
-const KnexSessionStore = KnexSessionStoreFactory(session);
+// const KnexSessionStore = KnexSessionStoreFactory(session);
 
-router.use(
-  session({
-    cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    },
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: new KnexSessionStore({
-      knex,
-      createtable: false,
-    }),
-  })
-);
+// router.use(
+//   session({
+//     cookie: {
+//       maxAge: 7 * 24 * 60 * 60 * 1000,
+//     },
+//     secret: SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: new KnexSessionStore({
+//       knex,
+//       createtable: false,
+//     }),
+//   })
+// );
 // router.use(passport.initialize());
 // router.use(passport.session());
 
