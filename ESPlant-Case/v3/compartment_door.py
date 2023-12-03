d51237dd54a64101de520928c85c82d0ee9b247f
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-12-03 16:58:25 +0000

    Case: wip recessed edge

--- ESPlant-Case/v3/compartment_door.py
+++ ESPlant-Case/v3/compartment_door.py
@@ -14,6 +14,7 @@ class CompartmentDoorSettings:
     :param fitting_arm_distance_factor: TODO
     :param tab_dimension: The dimensions of the tabs, meaning the width and height. Half of the height will be below the compartment door and the other half will stick out
                           The thickness of the tabs is the same as the thickness of the compartment door
+    :param recessed_edge_width: The width of the recessed edge. This is the amount of extra space on which the compartment door will be placed to not have it stick out and not have it fall in
     :param snap_joint_face_selectors: The face selectors of the faces of the compartment door where the snap joints should be placed. Uses the cadquery face selectors (eg. +Y means the face on the positive y axis)
                                       Multiple face selectors can be used by passing a list of face selectors
     :param tabs_face_selector: The face selector of the face of the compartment door where the tabs should be placed. Only one face can be selected. Uses the cadquery face selectors (eg. >Y means the face on the positive y axis)
@@ -28,6 +29,7 @@ class CompartmentDoorSettings:
     fitting_arm_angle_offset: float = 0.5
     tab_dimension: cq.Vector = (2, 3)
     tab_spacing_factor: float = 0.5
+    recessed_edge_width: float = 2
     snap_joint_overhang: float = 1
     snap_joint_face_selectors: List[Literal["+Y", "-Y",
                                             "+X", "-X"]] = field(default_factory=lambda: ["+Y"])
@@ -44,14 +46,10 @@ class CompartmentDoorTolerances:
     The tolerances for the compartment door
     :param tab_tolerance: The tolerance for the tabs. This is the amount of extra space where the tabs will be inserted
     :param fitting_arm_tolerance: The tolerance for the fitting arm. This is the amount of extra space (height and width) where the fitting arm will be inserted
-    :param recessed_edge_width: The width of the recessed edge. This is the amount of extra space on which the compartment door will be placed to not have it stick out and not have it fall in
-    :param recess_edge_depth: TODO: add description
     :param compartment_door_tolerance: The tolerance for the compartment door. This is the amount of extra space where the compartment door will be inserted
     """
     tab_tolerance: float = 0.5
     fitting_arm_tolerance: float = 0.5
-    recessed_edge_width: float = 2
-    recess_edge_depth: float = 5
     compartment_door_tolerance: float = 0.5
 
 
@@ -183,13 +181,31 @@ def generate_compartment_door_with_tolerance(settings: CompartmentDoorSettings =
             .box(s.tab_dimension[0] + 2 * t.tab_tolerance, s.compartment_door_dimensions.z + 2 * t.tab_tolerance, s.tab_dimension[1] + 2 * t.tab_tolerance, centered=(True, False, True))
             )
 
+def generate_compartment_door_frame(settings: CompartmentDoorSettings = CompartmentDoorSettings(), tolerances: CompartmentDoorTolerances = CompartmentDoorTolerances()) -> cq.Workplane:
+    s = settings
+    t = tolerances
+    compartment_door = generate_compartment_door(settings, tolerances)
+    compartment_door_with_tolerance = generate_compartment_door_with_tolerance(
+        settings, tolerances)
+    return (cq.Workplane(compartment_door_with_tolerance.faces("-Z", tag="compartment_door_with_tolerance").val())
+            .faces()
+            .wires()
+            .toPending()
+            .offset2D(-s.recessed_edge_width, "intersection")
+            .extrude(-s.compartment_door_dimensions.z)
+            .faces("|Z")
+            .shell(s.recessed_edge_width, "intersection")
+            )
+
 
 if __name__ == "__main__":
     from ocp_vscode import show_object
     compartmentDoorSettings = CompartmentDoorSettings(
-        snap_joint_face_selectors=["+X", "-X"], tabs_face_selector="<Y")
+        snap_joint_face_selectors=["+X", "-X", "+Y"], tabs_face_selector="<Y")
     compartmentDoorTolerances = CompartmentDoorTolerances()
     show_object(generate_compartment_door(
         compartmentDoorSettings), name="compartment_door")
     show_object(generate_compartment_door_with_tolerance(
         compartmentDoorSettings, compartmentDoorTolerances), name="compartment_door_with_tolerance")
+    show_object(generate_compartment_door_frame(
+        compartmentDoorSettings, compartmentDoorTolerances), name="compartment_door_frame")
