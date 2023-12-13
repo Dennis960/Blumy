6e7c8066dabc3aa5f270ad948189ca0e90a2c025
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2023-12-13 12:58:50 +0000

    Case: Fix _bool_op workarround by using inline imports

--- ESPlant-Case/v3/server/workarround_bool_op_error.py
+++
@@ -1,9 +0,0 @@
-import cadquery as cq
-_bool_op = cq.Shape._bool_op
-
-
-def _bool_op_no_parallel(self, args, tools, op, parallel = True):
-    return _bool_op(self, args, tools, op, False)
-
-
-cq.Shape._bool_op = _bool_op_no_parallel
