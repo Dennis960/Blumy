d2b0dd335d44f392cd79e682ea2acfbab8e7b995
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2024-03-28 13:14:51 +0000

    Casemaker: Update usb hole position and size

--- ESPlant-Casemaker/casemaker/esplant_default_settings.py
+++ ESPlant-Casemaker/casemaker/esplant_default_settings.py
@@ -3,10 +3,12 @@ from components import battery_springs
 
 
 board_settings = BoardSettings(part_settings=[
-    PartSetting(".*", ">Z", 0.5),
+    # PartSetting(".*", ">Z", 0.5),
     PartSetting(".*", "<Z", 0.5),
+    # TODO width and height should be the total width and height and they should overwrite the components width and height
+    # TODO offset should match original axes not the hole axes
     PartSetting(".*USB.*", "<X",
-                "Hole", width=11, height=6.5),
+                "Hole", width=13, height=6.5, offset_z=1),
     PartSetting(".*SW-SMD_L3.9-W3.0-P4.45.*", ">Z", "Hole"),
     PartSetting(".*SW-SMD_MK-12C02-G025.*", ">X", "Hole", height=4),
     PartSetting(".*LED.*", ">Z", "Hole"),
@@ -15,13 +17,15 @@ board_settings = BoardSettings(part_settings=[
     PartSetting(".*ESP32.*", ">Z", 2),
 ],
     parts_without_tolerances=["BatterySprings"],
+    part_tolerance=0.2,
     exclude=["PinHeader"]
 )
 case_settings = CaseSettings(
     case_dimension=("Auto", 55, 11),
     case_offset=(0, "Positive", "Positive")
 )
-compartment_door_settings = CompartmentDoorSettings(tab_spacing_factor=0.8, fitting_arm_height=11)
+compartment_door_settings = CompartmentDoorSettings(
+    tab_spacing_factor=0.8, fitting_arm_height=11)
 battery_holder_settings = BatteryHolderSettings(
     front_wall_thickness=2.5,
     back_wall_thickness=2,
