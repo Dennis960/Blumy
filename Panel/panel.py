0ee0e7070a58345f2c44f85d3e4ba6ba35e07fd7
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2024-09-03 18:18:24 +0000

    Remove Blumy-prefix

---
+++ Panel/panel.py
@@ -0,0 +1,118 @@
+from kikit import panelize_ui_impl as ki
+from kikit.fab import jlcpcb
+from kikit.units import mm, deg
+from kikit.panelize import Panel
+from pcbnewTransition.pcbnew import LoadBoard
+from itertools import chain
+
+# Custom config
+input = "Board/Board.kicad_pcb"
+output = "Board-Panel/panel.kicad_pcb"
+
+# KiKit Panel Config (Only deviations from default)
+
+layout = {
+    "hspace": "4mm",
+    "vspace": "2mm",
+    "cols": "2"
+}
+source = {
+    "tolerance": "15mm"
+}
+tabs = {
+    "type": "annotation",
+    "tabfootprints": "CustomFootprints:MouseBites5mm",
+    "vwidth": "5mm",
+    "hwidth": "5mm",
+    "vcount": "0"
+}
+cuts = {
+    "type": "mousebites",
+    "drill": "0.6mm",
+    "spacing": "1mm",
+    "offset": "-0.3mm"
+}
+framing = {
+    "type": "railslr",
+    "vspace": "5mm",
+    "hspace": "4mm"
+}
+tooling = {
+    "type": "4hole",
+    "hoffset": "2.5mm",
+    "voffset": "2.5mm",
+    "size": "1.152mm"
+}
+fiducials = {
+    "type": "4fid",
+    "hoffset": "4mm",
+    "voffset": "8mm",
+    "opening": "2mm"
+}
+text = {
+    "type": "simple",
+    "hoffset": "2mm",
+    "orientation": "90deg",
+    "text": "JLCJLCJLCJLC",
+    "anchor": "ml"
+}
+post = {
+    "dimensions": "True"
+}
+board = LoadBoard(input)
+panel = Panel(output)
+
+
+# Obtain full config by combining above with default
+preset = ki.obtainPreset([], layout=layout, tabs=tabs, cuts=cuts, framing=framing,
+                         tooling=tooling, fiducials=fiducials, text=text, post=post)
+
+
+# Register extra footprints for annotations
+for tabFootprint in preset["tabs"]["tabfootprints"]:
+    panel.annotationReader.registerTab(tabFootprint.lib, tabFootprint.footprint)
+
+panel.inheritDesignSettings(board)
+panel.inheritProperties(board)
+panel.inheritTitleBlock(board)
+
+sourceArea = ki.readSourceArea(preset["source"], board)
+substrates, framingSubstrates, backboneCuts = \
+    ki.buildLayout(preset, panel, input, sourceArea)
+
+tabCuts = ki.buildTabs(preset, panel, substrates, framingSubstrates)
+
+frameCuts = ki.buildFraming(preset, panel)
+
+ki.buildTooling(preset, panel)
+ki.buildFiducials(preset, panel)
+for textSection in ["text", "text2", "text3", "text4"]:
+    ki.buildText(preset[textSection], panel)
+ki.buildPostprocessing(preset["post"], panel)
+
+ki.makeTabCuts(preset, panel, tabCuts)
+ki.makeOtherCuts(preset, panel, chain(backboneCuts, frameCuts))
+
+ki.buildCopperfill(preset["copperfill"], panel)
+
+ki.setStackup(preset["source"], panel)
+ki.setPageSize(preset["page"], panel, board)
+ki.positionPanel(preset["page"], panel)
+
+ki.runUserScript(preset["post"], panel)
+
+ki.buildDebugAnnotation(preset["debug"], panel)
+
+panel.save(reconstructArcs=preset["post"]["reconstructarcs"],
+            refillAllZones=preset["post"]["refillzones"])
+
+footprints = panel.board.Footprints()
+for footprint in footprints:
+    reference = footprint.GetReference()
+    if "KiKit_" in str(reference):
+        footprint.SetExcludedFromBOM(True)
+        footprint.SetExcludedFromPosFiles(True)
+
+
+panel.save(reconstructArcs=preset["post"]["reconstructarcs"],
+           refillAllZones=preset["post"]["refillzones"])
--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 ## Project Structure
 
 - [.github](.github): Contains the configuration for GitHub Actions
-- [Blumy-Board](Blumy-Board): Contains the KiCad project for the board
-- [Blumy-Board-Panel](Blumy-Board-Panel): Contains the KiCad project for the board panel. Generation of the panel is done with `KiKit`
-- [Blumy-Firmware](Blumy-Firmware): Contains the firmware for the esp(32) sensor (uses esp-idf)
-- [Blumy-Pages](Blumy-Pages): Contains the website for the project's showcase
-- [Blumy-Server](Blumy-Server): Contains the website for displaying sensor data
+- [Board](Board): Contains the KiCad project for the board
+- [Panel](Panel): Contains the KiCad project for the board panel. Generation of the panel is done with `KiKit`
+- [Firmware](Firmware): Contains the firmware for the esp(32) sensor (uses esp-idf)
+- [Pages](Pages): Contains the website for the project's showcase
+- [Server](Server): Contains the website for displaying sensor data
