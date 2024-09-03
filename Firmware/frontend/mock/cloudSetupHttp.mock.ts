c54d28a3068094378472079df084b15b5258ef36
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2024-09-03 18:18:24 +0000

    Remove Blumy-prefix

---
+++ Firmware/frontend/mock/cloudSetupHttp.mock.ts
@@ -0,0 +1,10 @@
+import { defineMock } from "vite-plugin-mock-dev-server";
+
+export default defineMock({
+    url: "/api/cloudSetup/http",
+    body: {
+        sensorId: "45",
+        url: "http://blumy.cloud",
+        auth: "???",
+    },
+});
