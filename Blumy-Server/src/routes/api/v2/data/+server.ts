788bb5a3033a283833e0d1910aa1f9d0361934de
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2024-09-03 14:49:49 +0000

    Add blumy cloud api test endpoint

--- Blumy-Server/src/routes/api/v2/data/+server.ts
+++ Blumy-Server/src/routes/api/v2/data/+server.ts
@@ -1,22 +1,35 @@
-import SensorController from "$lib/server/controllers/SensorController";
-import type { ESPSensorReadingDTO } from "$lib/server/entities/SensorReadingEntity";
-import SensorRepository from "$lib/server/repositories/SensorRepository";
-import { json, type RequestHandler } from "@sveltejs/kit";
+import SensorController from '$lib/server/controllers/SensorController';
+import type { ESPSensorReadingDTO } from '$lib/server/entities/SensorReadingEntity';
+import SensorRepository from '$lib/server/repositories/SensorRepository';
+import { json, type RequestHandler } from '@sveltejs/kit';
 
 const dataController = new SensorController();
 
 export const POST = (async (event) => {
-    const authorizationHeader = event.request.headers.get("Authorization");
-    if (!authorizationHeader || !authorizationHeader.startsWith("Bearer ")) {
-        return json({ error: "Unauthorized" }, { status: 401 });
-    }
-    const writeToken = authorizationHeader.slice("Bearer ".length);
-    const sensorId = await SensorRepository.getIdByWriteToken(writeToken);
-    if (!sensorId) {
-        return json({ error: "Unauthorized" }, { status: 401 });
-    }
+	const authorizationHeader = event.request.headers.get('Authorization');
+	if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+		return json({ error: 'Unauthorized' }, { status: 401 });
+	}
+	const writeToken = authorizationHeader.slice('Bearer '.length);
+	const sensorId = await SensorRepository.getIdByWriteToken(writeToken);
+	if (!sensorId) {
+		return json({ error: 'Unauthorized' }, { status: 401 });
+	}
 
-    const sensorReading = await event.request.json() as ESPSensorReadingDTO;
-    const data = await dataController.addSensorData(sensorId, sensorReading);
-    return json(data);
+	const sensorReading = (await event.request.json()) as ESPSensorReadingDTO;
+	const data = await dataController.addSensorData(sensorId, sensorReading);
+	return json(data);
+}) satisfies RequestHandler;
+
+export const GET = (async (event) => {
+	const authorizationHeader = event.request.headers.get('Authorization');
+	if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+		return json({ error: 'Unauthorized' }, { status: 401 });
+	}
+	const writeToken = authorizationHeader.slice('Bearer '.length);
+	const sensorId = await SensorRepository.getIdByWriteToken(writeToken);
+	if (!sensorId) {
+		return json({ error: 'Unauthorized' }, { status: 401 });
+	}
+	return json({});
 }) satisfies RequestHandler;
