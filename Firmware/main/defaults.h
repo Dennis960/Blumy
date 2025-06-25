277c00a639b34feb6bf787e2586027942a854fc5
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2025-06-25 21:14:23 +0200

    Add automatic updates and fix factory reset not resetting wifi

--- Firmware/main/defaults.h
+++ Firmware/main/defaults.h
@@ -3,9 +3,11 @@
 #define DEFAULT_SENSOR_TIMEOUT_SLEEP_MS 1000 * 60 * 60 // 60 minutes
 #define DEFAULT_CONFIGURATION_MODE_TIMEOUT_MS 60000
 #define DEFAULT_WATCHDOG_TIMEOUT_MS 1000 * 20 // 20 seconds
+#define DEFAULT_OTA_TIMEOUT_MS 1000 * 60 * 10 // 10 minutes
 
 #define DEFAULT_SSID_MAX_LENGTH 33
 #define DEFAULT_PASSWORD_MAX_LENGTH 65
 #define DEFAULT_API_URL "http://blumy.cloud/api/v2/data"
+#define DEFAULT_FIRMWARE_UPDATE_URL "http://firmware.blumy.cloud/firmware/firmware.bin"
 
 #define DEFAULT_LED_BRIGHTNESS 128 // Default brightness for LEDs between 0 and 255
---
+++ Firmware/main/update.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_event.h"
+
+void update_updateFirmware(float *ota_update_percentage);
