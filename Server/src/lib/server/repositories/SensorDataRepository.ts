9654b471afde87608b7351793b883f9810f261ae
Dennis960 <66166638+Dennis960@users.noreply.github.com>
2025-07-31 14:30:44 +0000

    Move averaging the sensor data to the database query

--- Server/src/lib/server/repositories/SensorDataRepository.ts
+++ Server/src/lib/server/repositories/SensorDataRepository.ts
@@ -1,6 +1,6 @@
 import { sensorReadings } from '$lib/server/db/schema';
 import { db } from '$lib/server/db/worker';
-import { and, asc, count, desc, eq, gte, lte, sql } from 'drizzle-orm';
+import { and, asc, avg, count, desc, eq, gte, lte, max, min, sql } from 'drizzle-orm';
 
 const MAX_FIELD_CAPACITY = 2500;
 
@@ -30,57 +30,6 @@ export default class SensorDataRepository {
 	}
 
 	/**
-	 * Takes a list of data points and averages them to the specified limit.
-	 * The average of "water", "voltage", and "duration" are calculated.
-	 * The average of "date" is the last date.
-	 * If the limit is greater than the number of data points, the original data is returned.
-	 * @param data The list of data to average.
-	 * @param limit The final number of data points.
-	 * @returns The averaged data.
-	 */
-	private static dataToAverage(
-		data: (typeof sensorReadings.$inferSelect)[],
-		limit: number
-	): (typeof sensorReadings.$inferSelect)[] {
-		if (data.length <= limit) {
-			return data;
-		}
-		const averagedData: (typeof sensorReadings.$inferSelect)[] = [];
-		const step = Math.floor(data.length / limit);
-		for (let i = 0; i < data.length; i += step) {
-			const dataSlice = data.slice(i, i + step);
-
-			// Make a copy of the latest values
-			const averagedDataPoint = { ...dataSlice[dataSlice.length - 1] };
-
-			// Calculate averages in this bucket for summable attributes
-			const attributesToAverage = [
-				'light',
-				'voltage',
-				'temperature',
-				'humidity',
-				'moisture',
-				'moistureStabilizationTime',
-				'humidityRaw',
-				'temperatureRaw',
-				'rssi',
-				'duration'
-			] satisfies (keyof (typeof dataSlice)[0])[];
-
-			const calculateAverage = (property: keyof (typeof dataSlice)[0]) =>
-				dataSlice.reduce((acc, cur) => acc + ((cur[property] as number) ?? 0), 0) /
-				dataSlice.length;
-
-			for (const attribute of attributesToAverage) {
-				averagedDataPoint[attribute] = calculateAverage(attribute);
-			}
-
-			averagedData.push(averagedDataPoint);
-		}
-		return averagedData;
-	}
-
-	/**
 	 * Get data by sensor address. Newest data first.
 	 * @param sensorAddress The address of the sensor.
 	 * @param startDate The start date in ms since epoch. (optional)
@@ -93,24 +42,70 @@ export default class SensorDataRepository {
 		startDate: Date,
 		endDate: Date,
 		maxDataPoints: number
-	) {
+	): Promise<(typeof sensorReadings.$inferSelect)[]> {
 		// Fetch data between startDate and endDate
-		const data = (
-			await db
-				.select()
+		const startMs = startDate.getTime() / 1000;
+		const endMs = endDate.getTime() / 1000;
+		const bucket_duration = (endMs - startMs) / maxDataPoints;
+
+		const binned = db.$with('binned').as(
+			db
+				.select({
+					id: sensorReadings.id,
+					firmwareVersion: sensorReadings.firmwareVersion,
+					sensorAddress: sensorReadings.sensorAddress,
+					date: sensorReadings.date,
+					light: sensorReadings.light,
+					voltage: sensorReadings.voltage,
+					temperature: sensorReadings.temperature,
+					humidity: sensorReadings.humidity,
+					moisture: sensorReadings.moisture,
+					moistureStabilizationTime: sensorReadings.moistureStabilizationTime,
+					isMoistureMeasurementSuccessful: sensorReadings.isMoistureMeasurementSuccessful,
+					humidityRaw: sensorReadings.humidityRaw,
+					temperatureRaw: sensorReadings.temperatureRaw,
+					rssi: sensorReadings.rssi,
+					duration: sensorReadings.duration,
+					bin: sql<number>`floor((extract(epoch from ${sensorReadings.date}) - ${startMs}) / ${bucket_duration})`.as(
+						'bin'
+					)
+				})
 				.from(sensorReadings)
 				.where(
 					and(
+						sensorReadings.isMoistureMeasurementSuccessful,
 						eq(sensorReadings.sensorAddress, sensorAddress),
 						gte(sensorReadings.date, startDate),
 						lte(sensorReadings.date, endDate)
 					)
 				)
-				.orderBy(desc(sensorReadings.date))
-		).map(invertMoisture);
+		);
+		const data = await db
+			.with(binned)
+			.select({
+				id: min(sensorReadings.id),
+				firmwareVersion: max(sensorReadings.firmwareVersion),
+				sensorAddress: max(sensorReadings.sensorAddress),
+				date: sql<Date>`to_timestamp(${min(binned.bin)} * ${bucket_duration} + ${startMs})`.as(
+					'date'
+				),
+				light: sql<number>`avg(${sensorReadings.light})`,
+				voltage: sql<number>`avg(${sensorReadings.voltage})`,
+				temperature: sql<number>`avg(${sensorReadings.temperature})`,
+				humidity: sql<number>`avg(${sensorReadings.humidity})`,
+				moisture: sql<number>`avg(${sensorReadings.moisture})`,
+				moistureStabilizationTime: sql<number>`avg(${sensorReadings.moistureStabilizationTime})`,
+				isMoistureMeasurementSuccessful: sql<boolean>`true`,
+				humidityRaw: sql<number>`avg(${sensorReadings.humidityRaw})`,
+				temperatureRaw: sql<number>`avg(${sensorReadings.temperatureRaw})`,
+				rssi: sql<number>`avg(${sensorReadings.rssi})`,
+				duration: sql<number>`avg(${sensorReadings.duration})`,
+				bin: binned.bin
+			})
+			.from(binned)
+			.groupBy(({ bin }) => bin);
 
-		// Return the averaged data
-		return this.dataToAverage(data, maxDataPoints);
+		return data.map(invertMoisture);
 	}
 
 	static async getCountByWaterCapacityBucket(
